19b1e3daf5b8c63c20536491160215ef56e4948d
Christopher-0118 <k.karantsevich@gmail.com>
2019-11-06 23:58:05 +0300

    init: layout

---
+++ js/button.js
@@ -0,0 +1,8 @@
+class Button {
+  constructor(keyNum, keyEn, keyRu, keyShift) {
+    this.keyNum = keyNum;
+    this.keyEn = keyEn || null;
+    this.keyRu = keyRu || null;
+    this.keyShift = keyShift || null;
+  }
+}
